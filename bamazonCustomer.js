39318f47cccc2d5952996ae53440f901497a490d
Catherine Liu <catherine@Catherines-MacBook-Pro.local>
2017-07-11 22:39:47 -0700

    Installed mysql and inquirer packages. Added function to query all products. Added inquirer prompts.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,51 @@
-const inquire = require("inquiere");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 const Product = require("./product");
+
+const products = [];
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "bamazon"
+});
+
+connection.connect(function(err) {
+	if(err) throw err;
+	getProducts();
+});
+
+
+function getProducts() {
+	connection.query("SELECT * FROM products", function(err,res) {
+		for(let i = 0; i < res.length; i++) {
+			// console.log(res[i]);
+			let newProduct = Product(res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity);
+			console.log(newProduct.print());
+			products.push(newProduct);
+		}
+
+		userPrompt();
+	});
+}
+
+function userPrompt() {
+	inquirer.prompt([ {
+		type: "input",
+		name: "id",
+		message: "What is the ID # of the product you want to buy?"
+	}, {
+		type: "input",
+		name: "qty",
+		message: "How many units do you want to buy?"
+	}]).then(function(answers) {
+		let chosenItem = products.find(function(item) {
+			return item.id === answers.id;
+		});
+
+		console.log(chosenItem);
+	});
+
+}
--- product.js
+++ product.js
@@ -1,5 +1,8 @@
 const Product = function(id,name,dept,price,qty) {
 	let data = {id,name,dept,price,qty};
+	data.print = () => {
+		return `ID: ${data.id} \tName: ${data.name} \tDept: ${data.dept} \tPrice: \$${data.price.toFixed(2)} \tQuantity: ${data.qty}`;
+	};
 	return data;
 }
 
